73dbd191f77f4b8044b6fab415aa3e4835973b08
Lffyya <patrick.2005.joshua@gmail.com>
2025-07-29 19:48:51 +0530

    your commit message

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,10 +1,10 @@
 import { Navbar } from "@/components/navbar"
-import { HeroSection } from "@/pages/home/components/hero-section"
-import { AboutSection } from "@/pages/home/components/about-section"
-import { WhyBotAcademySection } from "@/pages/home/components/why-bot-academy-section"
-import { ProgramsSection } from "@/pages/home/components/programs-section"
-import { WhoShouldJoinSection } from "@/pages/home/components/who-should-join-section"
-import { JoinMovementSection } from "@/pages/home/components/join-movement-section"
+import { HeroSection } from "@/pages/home/hero-section"
+import { AboutSection } from "@/pages/home/about-section"
+import { WhyBotAcademySection } from "@/pages/home/why-bot-academy-section"
+import { ProgramsSection } from "@/pages/home/programs-section"
+import { WhoShouldJoinSection } from "@/pages/home/who-should-join-section"
+import { JoinMovementSection } from "@/pages/home/join-movement-section"
 import { Footer } from "@/components/footer"
 
 export default function HomePage() {
